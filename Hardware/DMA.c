045a53169b8887971540762c392ae7b56d527c6b
Tony_Huang <Tony_Huang@TONY_HUANG666>
2023-07-31 17:07:58 +0800

    错误只是因为把=写成了==，编译器不报错，肉眼也难以察觉

--- Hardware/DMA.c
+++ Hardware/DMA.c
@@ -3,7 +3,21 @@
 void DMA_Initilize(uint32_t A,uint32_t B,uint16_t Size)
 {
     RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
-   DMA_InitTypeDef DMA_InitStructure;
+    DMA_InitTypeDef DMA_InitStructure;
+    DMA_InitStructure.DMA_PeripheralBaseAddr = A;
+    DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+    DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Enable;
+    DMA_InitStructure.DMA_MemoryBaseAddr = B;
+    DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+    DMA_InitStructure.DMA_MemoryInc=DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_DIR=DMA_DIR_PeripheralSRC;
+    DMA_InitStructure.DMA_BufferSize=Size;
+    DMA_InitStructure.DMA_Mode=DMA_Mode_Normal;
+    DMA_InitStructure.DMA_M2M=DMA_M2M_Enable;
+    DMA_InitStructure.DMA_Priority=DMA_Priority_Medium;
+    DMA_Init(DMA1_Channel1, &DMA_InitStructure);//软件触发，通道可以任意选择
+    /*
+    DMA_InitTypeDef DMA_InitStructure;
 	DMA_InitStructure.DMA_PeripheralBaseAddr = A;
 	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
 	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Enable;
@@ -16,5 +30,6 @@ void DMA_Initilize(uint32_t A,uint32_t B,uint16_t Size)
 	DMA_InitStructure.DMA_M2M = DMA_M2M_Enable;
 	DMA_InitStructure.DMA_Priority = DMA_Priority_Medium;
 	DMA_Init(DMA1_Channel1, &DMA_InitStructure);
+    */
     DMA_Cmd(DMA1_Channel1, ENABLE);
 }
